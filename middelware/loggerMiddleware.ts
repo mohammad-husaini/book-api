7b7787e8b159b5e0a6477a65427c3edece0658aa
mohammad-husaini <mo.r.husini@gmail.com>
2023-07-24 14:21:16 +0300

    change path to url

--- middelware/loggerMiddleware.ts
+++ middelware/loggerMiddleware.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 const loggerMiddleware=(req:express.Request,res:express.Response,next:express.NextFunction)=>{
-    console.log(`${new Date().toLocaleString()} [${req.method}] ${req.path} `);
+    console.log(`${new Date().toLocaleString()} [${req.method}] ${req.url} `);
     next()
 }
 
