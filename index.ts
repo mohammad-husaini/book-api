4a3337b9f52d8ca6e30f1d82d695be446bc53d18
mohammad-husaini <mo.r.husini@gmail.com>
2023-08-20 11:02:28 +0300

    add dotenv and health check for aws

--- index.ts
+++ index.ts
@@ -1,13 +1,17 @@
 import express from 'express';
 import bookRoute from './routers/book.js';
 import loggerMiddleware from './middelware/loggerMiddleware.js';
-const app =express();
+import 'dotenv'
+const app = express();
 const PORT = 3000
 app.use(express.json());
-app.use('/',loggerMiddleware); 
-app.use('/books',bookRoute)
-app.use('/',(req,res)=>{res.status(404).send('Cant find this path sorry :( !');})
-app.listen(PORT,() => {
+app.use('/', loggerMiddleware);
+app.use('/books', bookRoute)
+app.get('/health', (req, res) => {
+    res.sendStatus(200);
+})
+app.use('/', (req, res) => { res.status(404).send('Cant find this path sorry :( !'); })
+app.listen(PORT, () => {
     console.log(`listening on PORT : ${PORT}`);
-    
+
 })
